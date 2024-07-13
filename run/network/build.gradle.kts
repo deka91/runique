04982a1242b48d0b8468245410b3e436de6cfbe4
Deniz Kalem <Deniz.Kalem@ti8m.ch>
2024-07-13 15:15:49 +0200

    Set up required dependencies

--- run/network/build.gradle.kts
+++ run/network/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
@@ -30,11 +30,6 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+    implementation(projects.core.domain)
+    implementation(projects.core.data)
 }
